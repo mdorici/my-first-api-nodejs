9f69f95f5c9c17a167bfeb32731fefae051b6b30
mdorici <marianadorici@gmail.com>
2022-12-21 20:58:30 -0300

    Add roles repository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,22 +1,13 @@
-import { Role } from '@roles/entities/Role'
+import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
 
 const rolesRouter = Router()
-
-const roles: Role[] = []
+const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body
+  const role = rolesRepository.create({ name })
 
-  const role = new Role()
-
-  Object.assign(role, {
-    name,
-    create_at: new Date(),
-  })
-
-  roles.push(role)
   return response.status(201).json(role)
 })
 
