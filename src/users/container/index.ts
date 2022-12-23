e858eb87b29e33fd8dad75002b2e0a4d909dac9f
mdorici <marianadorici@gmail.com>
2022-12-23 15:28:26 -0300

    Add CreateUserUseCase

---
+++ src/users/container/index.ts
@@ -0,0 +1,10 @@
+import { IUsersRepository } from '@users/repositories/IUsersRepository'
+import { UsersRepository } from '@users/repositories/UsersRepository'
+import { container } from 'tsyringe'
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+)
+
+container.registerSingleton()
