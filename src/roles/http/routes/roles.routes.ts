15d7c378bccc128786981e6abd82e243bf619b3c
mdorici <marianadorici@gmail.com>
2022-12-23 11:44:09 -0300

    Refactor roles routes

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,12 +1,18 @@
-import { createRolesController } from '@roles/useCases/createRole'
-import { deleteRolesController } from '@roles/useCases/deleteRole'
-import { listRolesController } from '@roles/useCases/listRoles'
-import { showRolesController } from '@roles/useCases/showRole'
-import { updateRolesController } from '@roles/useCases/updateRole'
 import { Router } from 'express'
 import { celebrate, Joi, Segments } from 'celebrate'
+import { container } from 'tsyringe'
+import { CreateRoleController } from '@roles/useCases/createRole/CreateRoleController'
+import { ListRolesController } from '@roles/useCases/listRoles/ListRolesController'
+import { ShowRoleController } from '@roles/useCases/showRole/ShowRoleController'
+import { UpdateRoleController } from '@roles/useCases/updateRole/UpdateRoleController'
+import { DeleteRoleController } from '@roles/useCases/deleteRole/DeleteRoleController'
 
 const rolesRouter = Router()
+const createRoleController = container.resolve(CreateRoleController)
+const listRolesController = container.resolve(ListRolesController)
+const showRoleController = container.resolve(ShowRoleController)
+const updateRolesController = container.resolve(UpdateRoleController)
+const deleteRoleController = container.resolve(DeleteRoleController)
 
 rolesRouter.post(
   '/',
@@ -16,7 +22,7 @@ rolesRouter.post(
     }),
   }),
   (request, response) => {
-    return createRolesController.handle(request, response)
+    return createRoleController.handle(request, response)
   },
 )
 
@@ -41,7 +47,7 @@ rolesRouter.get(
     }),
   }),
   (request, response) => {
-    return showRolesController.handle(request, response)
+    return showRoleController.handle(request, response)
   },
 )
 
@@ -68,7 +74,7 @@ rolesRouter.delete(
     }),
   }),
   (request, response) => {
-    return deleteRolesController.handle(request, response)
+    return deleteRoleController.handle(request, response)
   },
 )
 
