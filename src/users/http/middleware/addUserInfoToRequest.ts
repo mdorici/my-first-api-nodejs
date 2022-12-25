73e51af667c50a2a03f450e79d2e5e866ad5b065
mdorici <marianadorici@gmail.com>
2022-12-25 18:17:59 -0300

    Refactor meddleware is Authenticated

--- src/users/http/middleware/addUserInfoToRequest.ts
+++ src/users/http/middleware/addUserInfoToRequest.ts
@@ -1,13 +1,11 @@
-import { AppError } from '@shared/errors/AppError'
-import { Request, Response, NextFunction } from 'express'
-import { Secret, verify } from 'jsonwebtoken'
-import authConfig from '@config/auth'
+import { NextFunction, Request, Response } from 'express'
+import { decode } from 'jsonwebtoken'
 
 type JwtPayloadProps = {
   sub: string
 }
 
-export const isAuthenticated = (
+export const addUserInfoToRequest = (
   request: Request,
   response: Response,
   next: NextFunction,
@@ -29,14 +27,14 @@ export const isAuthenticated = (
     })
   }
   try {
-    const decodedToken = verify(token, authConfig.jwt.secret as Secret)
+    const decodedToken = decode(token)
     const { sub } = decodedToken as JwtPayloadProps
     request.user = { id: sub }
     return next()
   } catch {
     return response.status(401).json({
       error: true,
-      code: 'token.expired',
+      code: 'token.invalid',
       message: 'Access token not present',
     })
   }
